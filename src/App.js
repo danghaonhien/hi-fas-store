afe6df1c8e48a6aa5c89a0a5f66656c5ec5fc221
Nhien Dang <danghaonhien@gmail.com>
2020-08-11 19:27:58 -0700

    Added SignIn Page and styled Custom Button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component.jsx";
+
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-        </Switch>
-      </BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import "./header.styles.scss";
 import { Link } from "react-router-dom";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-const Header = () => {
-  return (
-    <div className='header'>
-      <Link className='logo-container' to='/'>
-        <Logo className='logo' />
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-      <div className='options'>
-        <Link className='option' to='/shop'>
-          SHOP
-        </Link>
-        <Link className='option' to='/contact'>
-          CONTACT
-        </Link>
-      </div>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
